03ecea2771b38050fa4da24ff25ab2a4dc56c29c
charDevMDP <char.developer@gmail.com>
2021-11-17 20:12:10 -0300

    merge modifications with branch develop

--- apps/app/urls.py
+++ apps/app/urls.py
@@ -4,5 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('categories/', views.Categories, name='categories'),
-    path('addCategory/', views.addCategoryForm, name='addCategory')
+    path('addCategory/', views.addCategoryForm, name='addCategory'),
+    path('', views.home, name="home"),
+    path('register/', views.register, name="register"),
 ]
