7ed29b7d87d3d3dd34cc64737097f601e195643d
charDevMDP <char.developer@gmail.com>
2021-11-17 17:15:37 -0300

    completed register and login user

--- apps/app/views.py
+++ apps/app/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth.views import LoginView
 from .forms import UserRegisterForm
-
+from django.contrib.auth import authenticate, login
 
 # Create your views here.
 
@@ -11,11 +10,20 @@ def home(request):
 
 def register(request):
     data = { 'form': UserRegisterForm() }
+    # si entra con el metodo POST
     if request.method == "POST":
         form = UserRegisterForm(request.POST)
         if form.is_valid():
+            # guardo en la db
             form.save()
+            # authenticar manualmente
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password1']
+            user = authenticate(username = username, password = password)
+            # inicio sesion usuario
+            login(request, user)
             return redirect("home")
         data['form'] = form
 
+    # sino entre como get normal
     return render(request, "registration/register.html", data)
