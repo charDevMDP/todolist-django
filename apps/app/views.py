9e472d72c3590617780983b05b0e7463d66533ae
charDevMDP <char.developer@gmail.com>
2021-11-18 13:05:03 -0300

    merge categories, ok

--- apps/app/views.py
+++ apps/app/views.py
@@ -1,12 +1,30 @@
 from django.shortcuts import render, redirect
-from .forms import UserRegisterForm
 from django.contrib.auth import authenticate, login
+from .forms import UserRegisterForm, CategoryForm
+
+from apps.app.models import Category
 
 # Create your views here.
 
 def home(request):
     return render(request, "home.html")
 
+def Categories(request):
+    # traigo todas las categorias de la db
+    categoriesList = Category.objects.all();
+    return render(request, 'listCategories.html', { 'listCat': categoriesList})
+
+
+def addCategory(request):
+    data = { 'form': CategoryForm()}
+    if request.method == 'POST':
+        form = CategoryForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('categories')
+        data['form'] = form
+
+    return render(request, 'addCategory.html', data)
 
 def register(request):
     data = { 'form': UserRegisterForm() }
